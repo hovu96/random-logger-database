d44bac401e75b78c3515e4383ba3a64704d58b17
Robert Fujara <robert.fujara@gmail.com>
2019-08-02 12:43:09 +0200

    -

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,13 @@ do
    I=$(shuf -i 1-4 -n 1)
    D=`date -Iseconds`
    case "$I" in
-      "1") echo "$D GET /index.html HTTP/1.0 200 2326"
+      "1") echo "$D Update 1 row in table 'shopping_card' HTTP/1.0 200 2326"
       ;;
-      "2") echo "$D GET /shop.html HTTP/1.0 200 6842"
+      "2") echo "$D Added 1 row to table 'card_item' HTTP/1.0 200 6842"
       ;;
-      "3") echo "$D POST /checkout HTTP/1.0 200 50"
+      "2") echo "$D Added 1 row to table 'card_item' HTTP/1.0 200 6842"
       ;;
-      "4") echo "$D GET /products/car.html HTTP/1.0 404 2326"
+      "4") echo "$D Removed 1 row from table 'card_item' HTTP/1.0 200 2326"
       ;;
    esac
 done
